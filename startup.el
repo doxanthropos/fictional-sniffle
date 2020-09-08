981116b1563b26e9f4bbe06610c618921ee0ec20
Knut Jackowski <doxanthropos@googlemail.com>
2020-09-08 21:59:35 +0200

    added center mode and no-bling

--- startup.el
+++ startup.el
@@ -21,3 +21,37 @@
 
 (global-set-key (kbd "C-M-ß") 'indent-region)
 
+;; no bling
+;; from:
+;; https://alexschroeder.ch/wiki/2020-08-26_Emacs_without_the_bling
+
+;; deactivate some bling
+(dolist (mode '(tool-bar-mode
+		blink-cursor-mode
+		menu-bar-mode
+		scroll-bar-mode))
+  (when (fboundp mode)
+    (funcall mode -1)))
+
+;; center mode
+;; also from here:
+;; https://alexschroeder.ch/wiki/2020-08-26_Emacs_without_the_bling
+
+;; center the window by adding a very wide fringe
+(define-minor-mode center-window-mode
+  "Minor mode to center the window using wide fringes."
+  :init-value nil
+  :global t
+  :group 'editing-basics
+  (if (not center-window-mode)
+      (set-fringe-style nil);; default
+    (set-fringe-mode
+     (/ (- (frame-pixel-width)
+           (* (+ fill-column 4)
+	      (frame-char-width)))
+        2))))
+
+(center-window-mode 1)
+
+;; Use F9 to toggle centering
+(global-set-key (kbd "<f9>") 'center-window-mode)
